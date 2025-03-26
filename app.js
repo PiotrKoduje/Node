6ada50ef816b8547d2efb16d64c48dd8d940a836
Piotr Pelc <piotrpelc83@gmail.com>
2025-03-26 21:28:54 +0100

    Data storage

--- app.js
+++ app.js
@@ -1,7 +1,61 @@
 const os = require('os');
 const fs = require('fs');
 
+// RAW DATA
+const genders = ['female', 'male'];
+const femaleNames = ['Mary', 'Patricia', 'Jennifer', 'Linda', 'Elizabeth', 
+	'Barbara', 'Susan', 'Jessica', 'Sarah', 'Emily'];
+const maleNames = ['James', 'John', 'William', 'Michael', 'David', 
+  'Richard', 'Joseph', 'Thomas', 'Charles', 'Daniel'];
+const lastNames = ['Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 
+  'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor', 
+  'Anderson', 'Thomas', 'Jackson', 'White', 'Harris', 
+  'Martin', 'Thompson', 'Garcia', 'Martinez', 'Robinson'];
+const people = [];
 
-// console.log('Platform: ', os.platform());
-// console.log('Arch: ', os.arch());
-// console.log('Info: ', os.userInfo());
+// UTILS FUNCTIONS
+const randChoice = (arr) => {
+	const index = Math.floor(Math.random() * arr.length);
+	return arr[index];
+}
+
+const generatePhoneNumber = () => {
+	let result = '+48';
+	for (let i = 0; i < 9; i++){
+		let digit = Math.floor(Math.random() * 9);
+		if (i % 3 == 0)
+			digit = `-${digit}`
+		result += digit;
+	}
+	return result;
+};
+
+const generateEmail = (firstName, lastName) => {
+	const result = `${ firstName[0].toLowerCase() + firstName.slice(1)}.${ lastName[0].toLowerCase() + lastName.slice(1) }@gmail.com`;
+	return result;
+};
+
+// GERERATE DATA
+for (let i = 0; i < 20; i++){
+	const gender = randChoice(genders);
+	const firstName = gender === 'female'? randChoice(femaleNames) : randChoice(maleNames);
+	const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+	const age = Math.floor(Math.random() * 61) + 18;
+	const email = generateEmail(firstName, lastName);
+	const phone = generatePhoneNumber();
+
+	const person = { gender, firstName, lastName, age, email, phone}
+	people.push(person);
+}
+
+const peopleJSON = JSON.stringify(people, null, 2);
+
+// DATA STORAGE
+fs.writeFile('people.json', peopleJSON, (err) => {
+	if (err) {
+		console.log('Error: ', err);
+	}	else {
+		console.log('Data has been saved');
+	}
+})
+
