2f74aa1c87397b8dede073e1317aeebf8a9133af
Piotr Pelc <piotrpelc83@gmail.com>
2025-03-26 19:12:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const os = require('os');
+const fs = require('fs');
+
+
+// console.log('Platform: ', os.platform());
+// console.log('Arch: ', os.arch());
+// console.log('Info: ', os.userInfo());
